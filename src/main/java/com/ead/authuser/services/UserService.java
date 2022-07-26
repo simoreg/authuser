274b01ef0c234cff61d6c9641eb73ea9640f22e6
simoreg <leandrosimoreg@gmail.com>
2022-07-26 07:42:10 -0300

    Serviços e endpoints para entidade user

--- src/main/java/com/ead/authuser/services/UserService.java
+++ src/main/java/com/ead/authuser/services/UserService.java
@@ -1,7 +1,19 @@
 package com.ead.authuser.services;
 
+import com.ead.authuser.models.UserModel;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public interface UserService {
+
+
+    List<UserModel> findAll();
+
+    Optional<UserModel> findById(UUID userId);
+
+    void delete(UserModel userModel);
 }
--- src/main/java/com/ead/authuser/services/impl/UserServiceImpl.java
+++ src/main/java/com/ead/authuser/services/impl/UserServiceImpl.java
@@ -1,7 +1,12 @@
 package com.ead.authuser.services.impl;
 
+import com.ead.authuser.models.UserModel;
 import com.ead.authuser.repositories.UserRepository;
 import com.ead.authuser.services.UserService;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public class UserServiceImpl implements UserService {
 
@@ -9,6 +14,21 @@ public class UserServiceImpl implements UserService {
 
     public UserServiceImpl (UserRepository userRepository){
         this.userRepository = userRepository;
+    }
+
+    @Override
+    public List<UserModel> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public Optional<UserModel> findById(UUID userId) {
+        return userRepository.findById(userId);
+    }
+
+    @Override
+    public void delete(UserModel userModel) {
+        userRepository.delete(userModel);
     }
 
 }
